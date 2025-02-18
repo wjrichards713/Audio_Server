c36ce17b571dfb1cbbe34cb0b74aa8acd38b60e3
Naveen Raghuvanshi <naveen@olecons.com>
2025-02-18 15:39:32 +0530

    update

--- server.js
+++ server.js
@@ -78,7 +78,11 @@ wss.on('connection', async (socket, req) => {
             allConnectedUsers.push(users[client.websocketId]);
           }
         });
-        socket.send({ users_connected: allConnectedUsers })
+        wss.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send({ users_connected: allConnectedUsers })
+          }
+        });
       }
       if(message.disconnect) {
         delete users[websocketId];
