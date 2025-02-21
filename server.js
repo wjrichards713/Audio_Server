382a16a94e0cbe69e7b7bee27621def3dfd006c9
Naveen Raghuvanshi <naveen@olecons.com>
2025-02-21 16:56:27 +0530

    update

--- server.js
+++ server.js
@@ -214,6 +214,6 @@ function decryptAES(encryptedData, key) {
   return decrypted;
 }
 
-setInterval(() => {
-  console.log({ udpSockets, members, udpClients, users });
-}, 10000);
+// setInterval(() => {
+//   console.log({ udpSockets, members, udpClients, users });
+// }, 10000);
