4304dea9414b9948d050a03b3e3c2855b375e9cc
Naveen Raghuvanshi <naveen@olecons.com>
2025-07-16 21:37:36 +0530

    update

--- server.js
+++ server.js
@@ -419,7 +419,7 @@ wss.on('connection', async (socket, req) => {
   const websocketId = queryParams.get('websocket_id');
   socket.websocketId = websocketId; // TODO remove this line not needed
   setInterval(() => {
-    socket.send(true);
+    socket.send([]);
   }, 50000);
   try {
     udpSockets[websocketId].address();
